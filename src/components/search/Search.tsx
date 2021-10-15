17d0fce591007f1dc58d15ceb7741635cdffca96
ajoalex012 <ajoalex012@gmail.com>
2021-10-15 20:21:20 +0530

    Search

---
+++ src/components/search/Search.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './style.scss'
+function Search() {
+    return (
+        <div className='search'>
+            <div className='search-box'>
+            <i className="fas fa-search"></i> <input placeholder='search or start new chat' type="text" name="" id="" />
+            </div>
+        </div>
+    )
+}
+
+export default Search
--- src/pages/homepage/Home.tsx
+++ src/pages/homepage/Home.tsx
@@ -4,12 +4,14 @@ import RightNamingPanel from '../../components/rightnamingpanel/RightNamingPanel
 import Message from '../../components/message/Message'
 import Compose from '../../components/compose/Compose'
 import LeftName from '../../components/leftname/LeftName'
+import Search from '../../components/search/Search'
 function Home() {
     return (
         <div className="container">
             <div className='home'>
             <div className="left-panel">
                 <LeftName />
+                <Search />
             </div>
             <div className="right-panel">
                 <RightNamingPanel />
